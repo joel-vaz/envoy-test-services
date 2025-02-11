3d00ee6bda87cc3b0d733bf0f98e21d52637a261
joel.vaz <joel.vaz@aircall.io>
2025-02-11 16:21:17 +0000

    feat: improving logging on the mesh

--- src/appB/app/config.py
+++ src/appB/app/config.py
@@ -3,4 +3,4 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'monitor-secret-key')
     LOG_LEVEL = os.environ.get('LOG_LEVEL', 'INFO')
-    TARGET_APP_URL = os.environ.get('TARGET_APP_URL', 'http://appa:5005') 
+    TARGET_APP_URL = "http://localhost:9901/calculate/add"  # Send through local proxy 
